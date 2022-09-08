6b03cd0cfc999fe74e8603fb1ac85b3082bc7b42
JIRAPAT SOISUWAN <jirapat_soi@cmu.ac.th>
2022-09-08 22:16:24 +0700

    0.0.1 (Change camera)

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -10,8 +10,11 @@ namespace Project1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D bGtile;
-        Texture2D farmer;
-        Vector2 pos;
+
+        public Texture2D farmer;
+        public Vector2 pos;
+        public Rectangle rec;
+
         int speed = 3;
         int direction = 0;
 
@@ -21,9 +24,13 @@ namespace Project1
         float timeperframe;
         float totalelapsed;
 
+        Camera camera;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 720;
+            _graphics.PreferredBackBufferHeight = 480;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -31,6 +38,7 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            camera = new Camera(GraphicsDevice.Viewport);
 
             base.Initialize();
         }
@@ -45,7 +53,7 @@ namespace Project1
             framepersec = 12;
             timeperframe = (float)1 / framepersec;
             totalelapsed = 0;
-            pos = new Vector2(20,270);
+            pos = new Vector2(720, 270);
             // TODO: use this.Content to load your game content here
         }
 
@@ -54,38 +62,56 @@ namespace Project1
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             ProcessInput();
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
             KeyboardState ks = Keyboard.GetState();
             {
                 if (ks.IsKeyDown(Keys.W))
                 {
-                    pos.Y = 270;
+                    //pos.Y = 270;
+                    pos.Y = pos.Y - speed;
+                    if (pos.Y <= 240)
+                    {
+                        pos.Y = 240;
+                    }
                     direction = 3;
                     UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
                 }
                 if (ks.IsKeyDown(Keys.S))
                 {
-                    pos.Y = 350;
+                    //pos.Y = 350;
+                    pos.Y = pos.Y + speed;
+                    if (pos.Y >= 380)
+                    {
+                        pos.Y = 380;
+                    }
                     direction = 0;
                     UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
                 }
                 if (ks.IsKeyDown(Keys.A))
                 {
                     pos.X = pos.X - speed;
+                    if (pos.X < 0)
+                    {
+                        pos.X = 0;
+                    }
                     direction = 1;
                     UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
                 }
                 if (ks.IsKeyDown(Keys.D))
                 {
                     pos.X = pos.X + speed;
+                    if (pos.X > 1415)
+                    {
+                        pos.X = 1415;
+                    }
                     direction = 2;
                     UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
                 }
             }
-
             // TODO: Add your update logic here
+
+            camera.Update(gameTime, this);
 
             base.Update(gameTime);
         }
@@ -93,7 +119,7 @@ namespace Project1
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, camera.transform);
             _spriteBatch.Draw(bGtile, new Vector2(0f, 0f), new Rectangle?(new Rectangle(0, 0, bGtile.Width, bGtile.Height)), Color.White);
             _spriteBatch.Draw(farmer, pos, new Rectangle(32 * frame, 48 * direction, 32, 48), (Color.White));
             // TODO: Add your drawing code here
